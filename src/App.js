5427fac25fea465c9a15e5c39f970dcd37d93771
ocralo <e_lopez1996@hotmail.com>
2019-05-22 10:55:38 -0500

    se acomodo la ruta, para redireccionar a la vista login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Loadable from "react-loadable";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import * as firebase from "firebase";
+
+const loading = () => (
+  <div className="rel-cargando">
+    <div id="loading" />
+  </div>
+);
+
+const Login = Loadable({
+  loader: () => import("./vista/Login/Login"),
+  loading
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" name="Login - Funmath" component={Login} />
+      </Switch>
+    </HashRouter>
   );
 }
 
