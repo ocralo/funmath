5a5e69852b926e82fac5f43b3b1ab08cee02cf06
ocralo <e_lopez1996@hotmail.com>
2019-06-01 14:26:23 -0500

    se crea la vista de ejercisios

--- src/views/Exercises/Exercises.js
+++ src/views/Exercises/Exercises.js
@@ -1,27 +1,51 @@
 import React, { Component } from "react";
 import "./Exercises.css";
 import Menu from "../../components/Menu/Menu";
+import Card from "../../components/Cards/Card";
 
 export default class Exercises extends Component {
   constructor(props) {
     super();
-    this.state = {};
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      name: "",
+      lastName: ""
+    };
+    this.changePage = this.changePage.bind(this);
   }
-  handleClick(e, dato) {
+  handleClick = event => {};
+
+  changePage(page) {
+    console.log(page);
+    this.props.history.push(page);
   }
 
   render() {
     return (
-     <section className="container-fluid h-100 w-100 m-0 p-0">
-        <Menu title="Ejercisios"/>
+      <section className="container-fluid h-100 w-100 m-0 p-0">
+        <Menu title="Ejercisios" />
         <div className="container">
-                <div className="row mt-5">
-                    
-                </div>
-            </div>
-            </section>
-                
+          <div className="row mt-5">
+            <Card
+              title="Ejercisio 1"
+              url="/home"
+              changePage={this.changePage}
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida congue aliquet. Donec convallis lacus vitae justo viverra sagittis. Praesent a mollis nunc, sit amet congue elit. "
+            />
+            <Card
+              title="Ejercisio 2"
+              url="gg2"
+              changePage={this.changePage}
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida congue aliquet. Donec convallis lacus vitae justo viverra sagittis. Praesent a mollis nunc, sit amet congue elit. "
+            />
+            <Card
+              title="Ejercisio 3"
+              url="gg3"
+              changePage={this.changePage}
+              content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi gravida congue aliquet. Donec convallis lacus vitae justo viverra sagittis. Praesent a mollis nunc, sit amet congue elit. "
+            />
+          </div>
+        </div>
+      </section>
     );
   }
 }
